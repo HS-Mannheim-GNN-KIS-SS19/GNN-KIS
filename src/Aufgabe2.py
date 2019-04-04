ca21d66bd1c9b26789e6f7746d700556460c4ec9
Firestar99 <31222740+firestar99@users.noreply.github.com>
2019-04-04 15:38:57 +0200

    [GNN-2] added training and main loop

--- src/Aufgabe2.py
+++ src/Aufgabe2.py
@@ -1,18 +1,53 @@
+from time import sleep
+
 import layer
 import matplotlib.pyplot as plt
 import numpy as np
-
 # model
+from math import sqrt
+
 model = layer.Model([
     layer.InputLayer((2,)),
-    layer.DenseLayer((4,)),
-    layer.DenseLayer((1,))
+    layer.DenseLayer((4,),
+                     fixed_values=True,
+                     weights=np.array([[1, 0, 2.8],
+                                       [-1, 0, 2.8],
+                                       [0, 1, 2.8],
+                                       [0, -1, 2.8]])
+                     ),
+    layer.DenseLayer((1,),
+                     # fixed_values=True,
+                     weights=np.array([[1, 1, 1, 1, 0]], dtype='float64')
+                     )
 ])
 
-# calc array
-out = np.hstack([np.array([model.run(np.array([x, y])) for x in range(-5, 5)]) for y in range(-5, 5)])
 
-# plt
-plt.imshow(out, interpolation='bilinear')
-plt.colorbar()
-plt.show()
+def generateXYDataset(function, min, max, step):
+    hstack = []
+    for x in np.arange(min, max, step):
+        vstack = []
+        for y in np.arange(min, max, step):
+            vstack.append(function(np.array([x, y])))
+            # vstack.append(0.8 if sqrt(x * x + y * y) < 1 else 0)
+        hstack.append(np.vstack(vstack))
+    return np.hstack(hstack)
+
+
+while (True):
+    # model.visualize(2)
+
+    # train
+    for x in np.arange(-2, 2, 0.05):
+        for y in np.arange(-2, 2, 0.05):
+            model.train(2, np.array([x, y]), np.array([0 if sqrt(x * x + y * y) < 1 else 0.8]), 0.01)
+
+    # calc array
+    out = generateXYDataset(model.run, -5, 5, 0.1)
+    # out = generateXYDataset(lambda xy: 0.8 if sqrt(xy[0] * xy[0] + xy[1] * xy[1]) < 1 else 0, -5, 5, 0.1)
+
+    # plt
+    plt.imshow(out, interpolation='bilinear')
+    plt.colorbar()
+    plt.show()
+
+    sleep(0.25)
