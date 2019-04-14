bug fixes with np matrix multiplication, fixed sigmoid exponent overflow, works with the mnist-dataset now

src/functions.py
@@ -1,8 +1,11 @@
 import numpy as np
+from scipy.special import expit
 
 
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x / 8.))
+    # use sigmoid function from scipy
+    return expit(x)
+   #return 1 / (1 + np.exp(-x/8.))
 
 
 def sigmoid_derivative(x):

src/layer.py
@@ -8,6 +8,7 @@ from src.functions import sigmoid, sigmoid_derivative
 # TODO for testing:
 np.random.seed(123)
 
+
 class Layer:
     def __init__(self, output_shape):
         self.output_shape = output_shape
@@ -62,6 +63,7 @@ class DenseLayer(Layer):
 
     def run(self, input):
         self.assert_input_shape(input.shape)
+        #ss= self.function(np.dot(self.weights, np.append(input, 1)))
         return self.function(np.dot(self.weights, np.append(input, 1)))
 
     def delta_learning(self, input, target, learn_rate):
@@ -90,12 +92,13 @@ class DenseLayer(Layer):
         if ek is None:
             # Backpropagation output layer
             ej = (oj - target) * oj * (1 - oj)
-            self.weights += -learn_rate * np.transpose(ej) * oi
+            self.weights += -learn_rate * np.mat(ej).T * oi
+
         else:
             # Backpropagation hidden layers
             # weights[:, :-1] deletes the last value/vector of the array/matrix (weight of bias neuron not needed here)
-            ej = ek * layers[len(o) + 1].weights[:, :-1] * oj * (1 - oj)
-            self.weights += -learn_rate * np.transpose(ej) * oi
+            ej = np.mat(ek) * (layers[len(o) + 1].weights[:, :-1] * oj * (1 - oj))
+            self.weights += -learn_rate * ej.T * oi
 
         # Until it reached the input layer
         if len(o) > 1:
@@ -147,10 +150,12 @@ class Model:
             curr_state = self.layers[i].run(curr_state)
         self.layers[layer_id].delta_learning(curr_state, target, learn_rate)
 
-    def backpropagation(self, input, target, learn_rate):
+    def backpropagation(self, input, target, learn_rate, return_output=False):
         cur = input
         output = []
         for layer in self.layers:
             output.insert(0, layer.run(cur))
             cur = output[0]
         self.layers[-1].backpropagation(self.layers, np.array(output), None, target, learn_rate)
+        if return_output:
+            return np.array(output)[0]

src/mnist_test.py
@@ -1,31 +1,29 @@
 import layer
 import numpy as np
-from src.GraphUtils import showColorMap
+import matplotlib.pyplot as plt
 
 model = layer.Model([
     layer.InputLayer((784,)),  # 28 x 28 images
     layer.DenseLayer((512,)),
     layer.DenseLayer((256,)),
     layer.DenseLayer((128,)),
-    layer.DenseLayer((10,))
+    layer.DenseLayer((10,))    # Outputs numbers from 0 - 9
 ])
 
 
-def backpropagation(i):
-    count = 0
-    while True:
-        if count == i:
-            # model.visualize(2)
+def visualize(input_val, target_number, output):
+    # Plot the original 28 x 28 image
+    plt.pcolormesh(np.arange(0, 28, 1), np.arange(0, 28, 1), np.flipud(np.array(input_val).reshape((28, 28))))
+    plt.colorbar()
+    plt.show()
 
-            count = 0
+    # Print each output neuron
+    for i, o in enumerate(output):
+        print("{}    {:1.6f}".format(i, o))
 
-        # train
-        for x in np.arange(-2, 2, 0.05):
-            for y in np.arange(-2, 2, 0.05):
-                pass
+    print("Predicted:  {}   Result:  {}".format(output.tolist().index(max(output)), target_number))
+    print("--------------")
 
-
-        count += 1
 
 def number_recognize(draw_each_i):
     from mlxtend.data import loadlocal_mnist
@@ -41,13 +39,19 @@ def number_recognize(draw_each_i):
 
     count = 0
     for input_val, target in zip(all_x, all_y):
-        model.backpropagation(input_val, target, 0.05)
+        # very important!
+        # target must not be the correct number but instead
+        # a vector with the height of the output layer where only the correct index is 1 (rest 0)
+        target_vec = np.zeros((10, ))
+        target_vec[target] = 1
 
         if count % draw_each_i == 0:
-            showColorMap(model, 0, 9, 0.05)
+            output = model.backpropagation(input_val, target_vec, 0.05, return_output=True)
+            visualize(input_val, target, output)
+        else:
+            model.backpropagation(input_val, target_vec, 0.05)
+
         count += 1
 
-a = np.array([1,2,1])
-b = np.array([2,2,3])
-print(np.dot(a,b))
-number_recognize(800)
+
+number_recognize(5000)